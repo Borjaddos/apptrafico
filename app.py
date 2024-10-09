16a931877fcf5d20d3222f9936a4a94299a7756f
Borja Diaz de Otazu <bdiaz@irontec.com>
2024-10-09 12:42:14 +0200

    Actualización del manejo de la conexión a la base de datos

--- app.py
+++ app.py
@@ -1,63 +1,42 @@
-from flask import Flask, jsonify, render_template, request
-import psycopg2
+from flask import Flask, jsonify, render_template
+import psycopg2 
 import os
 
-app = Flask(__name__)
+app = Flask(__name__)   
 
-# Usar la variable de entorno para la URL de la base de datos
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Ruta para obtener las localizaciones desde la base de datos
-@app.route('/get_locations')
-def get_locations():
-    # Conectar a la base de datos
+@app.route('/map_data/<fecha>')
+def map_data(fecha):
     conn = psycopg2.connect(DATABASE_URL)
     cur = conn.cursor()
-
-    # Ejecutar consulta para obtener los puntos geoespaciales
-    cur.execute("SELECT id, name, latitude, longitude FROM locations;")
-    locations = cur.fetchall()
-
-    # Cerrar la conexión
+    cur.execute("""
+        SELECT prediction, Description, X, Y 
+        FROM predicciones 
+        WHERE Fecha_Hora::date = %s
+    """, (fecha,))
+    
+    rows = cur.fetchall()
     cur.close()
     conn.close()
 
-    # Devolver los datos en formato JSON
-    return jsonify([{
-        'id': loc[0],
-        'name': loc[1],
-        'latitude': loc[2],
-        'longitude': loc[3]
-    } for loc in locations])
+    # Transformamos los datos en un formato adecuado
+    points = []
+    for row in rows:
+        prediction, description, x, y = row
+        points.append({
+            'prediction': prediction,
+            'description': description,
+            'x': x,
+            'y': y
+        })
 
-# Ruta para buscar localizaciones por nombre
-@app.route('/search_location', methods=['GET'])
-def search_location():
-    query = request.args.get('query', '')  # Obtener el parámetro de búsqueda
-
-    # Conectar a la base de datos
-    conn = psycopg2.connect(DATABASE_URL)
-    cur = conn.cursor()
-
-    # Ejecutar consulta con el parámetro de búsqueda
-    cur.execute("SELECT id, name, latitude, longitude FROM locations WHERE name ILIKE %s;", ('%' + query + '%',))
-    locations = cur.fetchall()
-
-    # Cerrar la conexión
-    cur.close()
-    conn.close()
-
-    # Devolver los resultados en formato JSON
-    return jsonify([{
-        'id': loc[0],
-        'name': loc[1],
-        'latitude': loc[2],
-        'longitude': loc[3]
-    } for loc in locations])
+    return jsonify(points)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
