8f71ea951b245b4a6c82aa81b9c76eb48e51d396
Borja Diaz de Otazu <bdiaz@irontec.com>
2024-10-09 19:04:43 +0200

    Dos pags

--- app.py
+++ app.py
@@ -84,5 +84,10 @@ def next_prediction():
 
     return jsonify(prediction)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
